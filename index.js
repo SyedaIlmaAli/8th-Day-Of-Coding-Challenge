69a7ede134a8b39b881d03e5fefc55f59487044f
SyedaIlmaAli <160448031+SyedaIlmaAli@users.noreply.github.com>
2024-05-27 03:23:44 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+"use strict";
+//Day 08
+//Question 22
+let errorArr = ["Ilma", "Mehak", "Asma", "Hamna"];
+console.log(errorArr[5]); // Intentional array
+errorArr[2] = "Asma"; //  Correcting the error by accessing a valid index.
+// Question 23
+let country = "Pakistan";
+console.log("Is country == 'Pakistan'? I predict True.");
+console.log(country == 'Pakistan');
+console.log("Is country == 'England'? I predict False");
+console.log(country == 'England');
+console.log("Is country == 'South Korea'? I predict False");
+console.log(country == 'South Korea');
+console.log("Is country == 'India'? I predict False");
+console.log(country == 'India');
+console.log("Is country == 'Austrailia'? I predict False");
+console.log(country == 'Austrailia');
+// Question 24
+console.log("Testing with strings");
+console.log("apple" == "apple");
+console.log("Testing With the lower case function");
+console.log("Apple".toLowerCase() == "apple");
+console.log("Numberical Test");
+console.log(10 > 5);
+console.log(1 > 5);
+console.log("Test with 'and' and 'or'.");
+console.log(true && false);
+console.log(true || false);
